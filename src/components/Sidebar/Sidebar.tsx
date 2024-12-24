badfb77b491b4c7262fc4cde9537cfd0f573b46a
paulohlips <paulohfcmendes@gmail.com>
2024-12-24 16:23:55 -0300

    finishing sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,11 +9,14 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
             />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/paulohlips.png" />
+
                 <strong>Diego Fernandes</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
