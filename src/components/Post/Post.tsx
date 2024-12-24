1147aa5436b36405e8e880fb63d2d13d7fa6e821
paulohlips <paulohfcmendes@gmail.com>
2024-12-24 16:35:25 -0300

    creating post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,13 +1,30 @@
-type PostProps = {
-    author: string,
-    content: string
-}
+import styles from './Post.module.css';
 
-export const Post = ({ author, content }: PostProps) => {
+export function Post() {
     return (
-        <div>
-            <p>Author: {author}</p>
-            <p>{content}</p>
-        </div>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/paulohlips.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Paulo Mendes</strong>
+                        <span>Fullstack Developer</span>
+                    </div>
+                </div>
+
+                <time title="11 de Maio às 08:13h" dateTime="2025-05-11 08:13:00">One hour agor</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Title 👋</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem libero voluptate magni autem deleniti adipisci eligendi molestias aut, qui cupiditate ratione veritatis perferendis id explicabo laborum veniam sint omnis eos? 🚀</p>
+                <p><a href="">pauloiscoding.com</a></p>
+                <p>
+                    <a href="">#newproject</a>{' '}
+                    <a href="">#coding</a>{' '}
+                    <a href="">#react</a>
+                </p>
+            </div>
+        </article>
     )
 }
